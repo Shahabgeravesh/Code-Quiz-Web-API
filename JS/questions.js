e42070356fdf212b98c1756dde4f434efb4be50e
Shahabgeravesh <Shahabgeravesh@gmail.com>
2021-09-28 18:52:12 -0700

    Quiz HTML, CSS, JS

---
+++ JS/questions.js
@@ -0,0 +1,88 @@
+//Collaborated with Janvier Mbilizi
+
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "What will be the output of the following code: print type(type(int))",
+    choices: ["type int", "type type", "Error", "Zero"],
+    answer: "type type"
+  },
+  {
+    title: "WWhat is the output of the following program : y = 8 z = lambda x : x * y print (z(6)) ",
+    choices: [
+      "48",
+      "14",
+      "64",
+      "None of the above"
+    ],
+    answer: "48"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "Boolean operators that can be used in JavaScript include:",
+    choices: [
+      "'And' Operator &&",
+      "'Or' Operator ||",
+      "'Not' Operator !",
+      "All the above"
+    ],
+    answer: "All the above"
+  },
+  {
+    title:
+      "What is called when a function is defined inside a class?",
+    choices: [
+      "Module",
+      "Class",
+      "Another Function",
+      "Method"
+    ],
+    answer: "Method"
+  },
+  {
+    title: "What is the data type of variables in JavaScript?",
+    choices: [
+      "Object data types",
+      "Function data type",
+      "None of the above",
+      "All of the above"
+    ],
+    answer: "Object data types"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "Which of the following is the use of id() function in python?.",
+    choices: ["Id returns the identity of the object", "Every object doesn't have a unique id", "All of the mentioned", "None of the mentioned"],
+    answer: "Id returns the identity of the object"
+  },
+  {
+    title:
+      "Consider the results of a medical experiment that aims to predict whether someone is going to develop myopia based on some physical measurements and heredity. In this case, the input dataset consists of the person’s medical characteristics and the target variable is binary: 1 for those who are likely to develop myopia and 0 for those who aren’t. This can be best classified as",
+    choices: ["Regression", "Decision Tree", "Clustering", "Association rules"],
+    answer: "console.log"
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above"
+  }
+];
--- README.md
+++
@@ -1,1 +0,0 @@
-# Code-Quiz-Web-API
